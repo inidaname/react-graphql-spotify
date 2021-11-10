e760f0bad90f9fb62c963ab68e748df013857a81
Hassan Sani <saniyhassan@gmail.com>
2021-11-10 15:34:48 +0100

    add test for axios call on search

--- src/components/search/Search.test.tsx
+++ src/components/search/Search.test.tsx
@@ -1,12 +1,22 @@
-import React from "react";
-import { render, cleanup, fireEvent, screen } from "@testing-library/react";
+import React, { useState } from "react";
+import { render, fireEvent, screen, act } from "@testing-library/react";
 import { Search } from "..";
 import App from "../../App";
+import mocks from "../../tests/mock/result";
+import userEvent from "@testing-library/user-event";
+import axios from 'axios';
+
 
 describe("Search component", () => {
+  let mockJest: typeof jest;
   beforeEach(() => {
     render(<App />);
   });
+
+  beforeAll(() => {
+    mockJest = jest.mock('axios')
+  });
+
 
   it("should allow input event on search input", () => {
     const input: HTMLInputElement = screen.getByRole("searchbox", {
@@ -14,5 +24,15 @@ describe("Search component", () => {
     }) as HTMLInputElement;
     fireEvent.change(input, { target: { value: "Kendrik" } });
     expect(input.value).toBe("Kendrik");
+  });
+
+  it("should mock search result", async () => {
+    const getSpy = mockJest.spyOn(axios, 'get');
+    const input: HTMLInputElement = screen.getByRole("searchbox", {
+      name: "Search by artist name",
+    }) as HTMLInputElement;
+    fireEvent.change(input, { target: { value: "Eminem" } });
+    expect(getSpy).toBeCalled()
+
   });
 });
