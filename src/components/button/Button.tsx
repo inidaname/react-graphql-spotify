06f94d8133aede7aa8debe0b1ec12ea0cacbb0d3
Hassan Sani <saniyhassan@gmail.com>
2021-11-15 21:45:55 +0100

    add css styles for button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -8,7 +8,7 @@ import AppContext from '../../context';
 const Button: FunctionComponent<ButtonProps> = ({handleClick}) => {
   const {state} = useContext<ContextState>(AppContext)
   console.log(state)
-  return  <button className={styles.button} disabled={state} name="search" onClick={handleClick}>Search</button>;
+  return  <button className={styles.button} disabled={state} name="search" onClick={handleClick}>Start Search</button>;
 };
 
 export default Button;
