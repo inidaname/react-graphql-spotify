d83ad77c88cce03eee4b2dfe13939289f7b38069
Hassan Sani <saniyhassan@gmail.com>
2021-11-15 15:25:12 +0100

    add react helmet to access document header to help add google fonts to the application without editing the initial index.html provided

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import React, {
 } from "react";
 import { Button, Footer, Header, Result, Search } from "./components";
 import { useArtistLazyQuery } from "./generated/graphql";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const App: FunctionComponent = (): ReactElement => {
   // set values for search input
@@ -14,15 +15,15 @@ const App: FunctionComponent = (): ReactElement => {
 
   // const [getArtists, { data, error }] = useLazyQuery(QUERY_ARTISTS);
 
-  const [getArtists, {data, loading, error}] = useArtistLazyQuery();
+  const [getArtists, { data, loading, error }] = useArtistLazyQuery();
 
   const updateQuery = () => {
     getArtists({ variables: { byName: values } });
   };
-  
+
   // function getSearch() {
-    //   setPoplate(values);
-    // }
+  //   setPoplate(values);
+  // }
 
   // add graphql query
   // if (loading) return <>"Loading..."</>;
@@ -30,11 +31,22 @@ const App: FunctionComponent = (): ReactElement => {
 
   return (
     <>
-      <Header />
-      <Search values={values} handleChange={setValues} />
-      <Button status={loading} handleClick={updateQuery} />
-      {data && <Result data={data} />}
-      <Footer />
+      <HelmetProvider>
+        <Helmet>
+          <title>Music Graphql Search</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500&display=swap"
+            rel="stylesheet"
+          />
+        </Helmet>
+        <Header />
+        <Search values={values} handleChange={setValues} />
+        <Button status={loading} handleClick={updateQuery} />
+        {data && <Result data={data} />}
+        <Footer />
+      </HelmetProvider>
     </>
   );
 };
